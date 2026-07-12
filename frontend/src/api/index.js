aedb3fadcc827e8a50b588f05bcc3f05e9d452af
hiir-21 <hiir.j@ahduni.edu.in>
2026-07-12 16:38:41 +0530

    fixed API URLs

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,21 +4,21 @@ const API = axios.create({
   baseURL: 'https://transitops-backend-4hhh.onrender.com'
 })
 
-export const getVehicles = () => API.get('/api/vehicles/vehicles/')
-export const createVehicle = (data) => API.post('/api/vehicles/vehicles/', data)
+export const getVehicles = () => API.get('/api/vehicles/')
+export const createVehicle = (data) => API.post('/api/vehicles/', data)
 
-export const getDrivers = () => API.get('/api/drivers/drivers/')
-export const createDriver = (data) => API.post('/api/drivers/drivers/', data)
+export const getDrivers = () => API.get('/api/drivers/')
+export const createDriver = (data) => API.post('/api/drivers/', data)
 
-export const getTrips = () => API.get('/api/trips/trips/')
-export const createTrip = (data) => API.post('/api/trips/trips/', data)
-export const updateTrip = (id, data) => API.patch(`/api/trips/trips/${id}/`, data)
+export const getTrips = () => API.get('/api/trips/')
+export const createTrip = (data) => API.post('/api/trips/', data)
+export const updateTrip = (id, data) => API.patch(`/api/trips/${id}/`, data)
 
-export const getMaintenance = () => API.get('/api/maintenance/maintenance/')
-export const createMaintenance = (data) => API.post('/api/maintenance/maintenance/', data)
-export const updateMaintenance = (id, data) => API.patch(`/api/maintenance/maintenance/${id}/`, data)
+export const getMaintenance = () => API.get('/api/maintenance/')
+export const createMaintenance = (data) => API.post('/api/maintenance/', data)
+export const updateMaintenance = (id, data) => API.patch(`/api/maintenance/${id}/`, data)
 
-export const getFuel = () => API.get('/api/fuel/fuel/')
-export const createFuel = (data) => API.post('/api/fuel/fuel/', data)
+export const getFuel = () => API.get('/api/fuel/')
+export const createFuel = (data) => API.post('/api/fuel/', data)
 
 export default API
