c297142a277c96780b498929b949dd89fe727eb3
prachipatel4102 <prachipatel4102@gmail.com>
2026-07-12 13:15:12 +0530

    updated urls

--- backend/transitops/urls.py
+++ backend/transitops/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('vehicles.urls')),
-    path('api/', include('drivers.urls')),
-    path('api/', include('trips.urls')),
-    path('api/', include('maintenance.urls')),
-    path('api/', include('fuel.urls')),
+    path('api/vehicles/', include('vehicles.urls')),
+    path('api/drivers/', include('drivers.urls')),
+    path('api/trips/', include('trips.urls')),
+    path('api/maintenance/', include('maintenance.urls')),
+    path('api/fuel/', include('fuel.urls')),
 ]
